212a55c42736b0ee87e8bb9f82d849cc02e558cc
backyardpartybr <101026007+backyardpartybr@users.noreply.github.com>
2023-06-29 15:34:59 -0500

    Update telegram-bot.js

--- telegram-bot.js
+++ telegram-bot.js
@@ -1,33 +1,35 @@
 const TelegramBot = require('node-telegram-bot-api');
-const telegramGroups = require('telegram-groups');
 
-// Replace "YOUR_BOT_TOKEN" with your actual bot token
-const bot = new TelegramBot('6337995792:AAFuDK2DhsJWbHqk31zJRiu6SSoNxs6XFwM', { polling: true });
-
-bot.onText(/\/start/, (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Hello! Welcome to the Telegram Group and Channel Search bot. Please use the /search command to search for groups and channels.');
-});
+// Replace 'YOUR_API_TOKEN' with the API token provided by BotFather
+const bot = new TelegramBot('AAFuDK2DhsJWbHqk31ZJRiu6SSoNxs6XFwM', { polling: true });
 
 bot.onText(/\/search (.+)/, (msg, match) => {
   const chatId = msg.chat.id;
   const keyword = match[1];
 
-  if (keyword) {
-    telegramGroups.searchGroups(keyword)
-      .then((results) => {
-        if (results.length > 0) {
-          const reply = results.join('\n');
-          bot.sendMessage(chatId, reply);
-        } else {
-          bot.sendMessage(chatId, `No groups or channels found matching the keyword "${keyword}".`);
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-        bot.sendMessage(chatId, 'An error occurred while searching for groups and channels.');
-      });
-  } else {
-    bot.sendMessage(chatId, 'Please provide a keyword to search for groups and channels.');
-  }
+  // Get the list of all channels and groups the bot is a member of
+  bot.getChatList().then(chats => {
+    let results = [];
+
+    chats.forEach(chat => {
+      if (chat.title.toLowerCase().includes(keyword.toLowerCase())) {
+        results.push(chat.title);
+      }
+    });
+
+    if (results.length === 0) {
+      bot.sendMessage(chatId, 'No results found.');
+    } else {
+      bot.sendMessage(chatId, `Results:\n${results.join('\n')}`);
+    }
+  }).catch(error => {
+    console.error(error);
+    bot.sendMessage(chatId, 'An error occurred while searching for channels and groups.');
+  });
+});
+
+// Start the bot
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Bot started. Use the /search command followed by a keyword to search for channels and groups.');
 });
